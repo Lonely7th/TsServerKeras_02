1bcd3c57c717e5d6cc62ddcce8a6e1e9523db7b2
dusting <1003882179@qq.com>
2018-03-19 11:26:54 +0800

    20180319

---
+++ k_backtest/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+__title__ = ''
+__author__ = 'JN Zhang'
+__mtime__ = '2018/3/19'
+"""
+
+
+'''
+回测系统
+
+'''
---
+++ k_spiser/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+__title__ = ''
+__author__ = 'JN Zhang'
+__mtime__ = '2018/3/19'
+"""
+
+
+'''
+数据爬虫
+这个模块将为逻辑思维系统和回测系统提供数据
+'''
